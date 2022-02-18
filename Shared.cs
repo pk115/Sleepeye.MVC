3aae17464312732d2e7f1bcca82e6dddacc6289d
Tongchai Bamrungsee <Tongchai.bam@somboon.co.th>
2022-02-18 14:06:01 +0700

    เพิ่มฟังชั่น

--- Shared.cs
+++ Shared.cs
@@ -5,12 +5,26 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Sleepeye.MVC
 {
     public class Shared
     {
-
+        public static string EnryptString(string strEncrypted)
+        {
+            // return strEncrypted;
+            byte[] b = ASCIIEncoding.UTF8.GetBytes(strEncrypted);
+            string encryptedPassword = Convert.ToBase64String(b);
+            return encryptedPassword;
+        }
+        public static string DecryptString(string encrString)
+        {
+            // return encrString;
+            byte[] b = Convert.FromBase64String(encrString);
+            string decryptedPassword = ASCIIEncoding.UTF8.GetString(b);
+            return decryptedPassword;
+        }
         public static string Encrypt(string key, string data)
         {
             string encData = null;
@@ -131,15 +145,343 @@ namespace Sleepeye.MVC
             return plaintext;
         }
 
-        public class JsonResponse
-        {
-            public bool status { get; set; }
-            public object data { get; set; }
-            public int total { get; set; }
-            public string message { get; set; }
-        }
-
-
-
+        //public void FileToByte(HttpPostedFile Files)
+        //{
+        //    using (var binaryReader = new BinaryReader(Files.InputStream))
+        //    {
+        //        var Bytes = binaryReader.ReadBytes(Files.ContentLength);
+        //    }
+        //}
     }
+    public class JsonResponse
+    {
+        public bool status { get; set; }
+        public object data { get; set; }
+        public int total { get; set; }
+        public string message { get; set; }
+    }
+
+    public class SelectOption
+    {
+        public object Value { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class JsonDataTable
+    {
+        public bool status { get; set; }
+        public string message { get; set; }
+        public int draw { get; set; }
+        public int recordsTotal { get; set; }
+        public int recordsFiltered { get; set; }
+        public object data { get; set; }
+    }
+
+    public class FilterModel
+    {
+        public int CurrentPage { get; set; }
+        public string Query { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Database { get; set; }
+        public List<string> Status { get; set; }
+        public int Docnum { get; set; }
+        public string Disable { get; set; }
+        public List<string> ProvinceCode { get; set; }
+        public List<string> UserGroupCode { get; set; }//pink20171120
+        public List<string> DocType { get; set; }
+        public List<string> Product { get; set; }
+        public List<string> Size { get; set; }
+        public List<string> OrderNo { get; set; }
+
+        public List<string> Supplier { get; set; }
+        public string Query2 { get; set; }
+        public string InOut { get; set; }
+        public DateTime? ExpiredDateStart { get; set; }
+        public DateTime? ExpiredDateEnd { get; set; }
+        public string DealerTypecode { get; set; }
+        public List<int> Year { get; set; }
+        public List<int> Month { get; set; }
+        public string Branch_Code { get; set; }
+        public string Company_Code { get; set; }
+        public string StockCode { get; set; }
+        public string Color_Code { get; set; }
+        public List<string> ColorCode { get; set; }
+
+        public FilterModel()
+        {
+            CurrentPage = 1;
+            Status = new List<string>();
+            ProvinceCode = new List<string>();
+            UserGroupCode = new List<string>();//pink20171120
+            DocType = new List<string>();
+            Product = new List<string>();
+            ColorCode = new List<string>();
+            Supplier = new List<string>();
+            OrderNo = new List<string>();
+            Size = new List<string>();
+            Year = new List<int>();
+            Month = new List<int>();
+
+
+
+        }
+    }
+
+    public class DatableOption : FilterModel
+    {
+        public int start { get; set; }
+        public int length { get; set; }
+        public int draw { get; set; }
+        public List<Dictionary<string, string>> order { get; set; }
+        private int sorting => int.Parse(order[0].FirstOrDefault(r => r.Key == "column").Value);
+        private string dir => order[0].FirstOrDefault(r => r.Key == "dir").Value;
+
+        public string ApplySorting(string[] column)
+        {
+            if (length == -1)
+                length = 100;
+
+            var s = sorting;
+            if (s >= column.Length)
+                s = column.Length - 1;
+            return $"{column[s]} {dir}";
+        }
+    }
+
+    public class NumberToEnglish
+    {
+        public String changeNumericToWords(double numb)
+        {
+            String num = numb.ToString();
+            return changeToWords(num, false);
+        }
+        public String changeCurrencyToWords(String numb)
+        {
+            return changeToWords(numb, true);
+        }
+        public String changeNumericToWords(String numb)
+        {
+            return changeToWords(numb, false);
+        }
+        public String changeCurrencyToWords(double numb)
+        {
+            return changeToWords(Math.Round(numb, 2).ToString("0.00"), true);
+        }
+        private String changeToWords(String numb, bool isCurrency)
+        {
+            String val = "", wholeNo = numb, points = string.Empty, andStr = string.Empty, pointStr = string.Empty;
+            String endStr = (isCurrency) ? ("Only") : ("");
+            try
+            {
+                int decimalPlace = numb.IndexOf(".");
+                int negative = numb.IndexOf("-");
+                if (decimalPlace > 0)
+                {
+                    wholeNo = numb.Substring(0, decimalPlace);
+                    points = numb.Substring(decimalPlace + 1);
+                    if (Convert.ToInt32(points) > 0)
+                    {
+                        andStr = (isCurrency) ? ("and") : ("point");// just to separate whole numbers from points/cents
+                        endStr = (isCurrency) ? ("Stang  " + endStr) : ("");
+                        pointStr = tens(points);
+                    }
+                }
+                if (negative > -1)
+                {
+                    wholeNo = numb.Substring(negative + 1);
+                }
+                val = String.Format("{0} {1} {2} {3} {4}", translateWholeNumber(wholeNo).Trim(), "Baht", andStr, pointStr, endStr);
+            }
+            catch {; }
+            return val;
+        }
+        private String translateWholeNumber(String number)
+        {
+            string word = "";
+            try
+            {
+                bool beginsZero = false;//tests for 0XX
+                bool isDone = false;//test if already translated
+                double dblAmt = (Convert.ToDouble(number));
+                //if ((dblAmt > 0) && number.StartsWith("0"))
+                if (dblAmt > 0)
+                {//test for zero or digit zero in a nuemric
+                    beginsZero = number.StartsWith("0");
+                    int numDigits = number.Length;
+                    int pos = 0;//store digit grouping
+                    String place = "";//digit grouping name:hundres,thousand,etc...
+                    switch (numDigits)
+                    {
+                        case 1://ones' range
+                            word = ones(number);
+                            isDone = true;
+                            break;
+                        case 2://tens' range
+                            word = tens(number);
+                            isDone = true;
+                            break;
+                        case 3://hundreds' range
+                            pos = (numDigits % 3) + 1;
+                            place = " Hundred ";
+                            break;
+                        case 4://thousands' range
+                        case 5:
+                        case 6:
+                            pos = (numDigits % 4) + 1;
+                            place = " Thousand ";
+                            break;
+                        case 7://millions' range
+                        case 8:
+                        case 9:
+                            pos = (numDigits % 7) + 1;
+                            place = " Million ";
+                            break;
+                        case 10://Billions's range
+                            pos = (numDigits % 10) + 1;
+                            place = " Billion ";
+                            break;
+                        //add extra case options for anything above Billion...
+                        default:
+                            isDone = true;
+                            break;
+                    }
+                    if (!isDone)
+                    {//if transalation is not done, continue...(Recursion comes in now!!)
+                        word = translateWholeNumber(number.Substring(0, pos)) + place + translateWholeNumber(number.Substring(pos));
+                        //check for trailing zeros
+                        if (beginsZero) word = " and " + word.Trim();
+                    }
+                    //ignore digit grouping names
+                    if (word.Trim().Equals(place.Trim())) word = "";
+                }
+            }
+            catch {; }
+            return word.Trim();
+        }
+        private String tens(String digit)
+        {
+            int digt = Convert.ToInt32(digit);
+            String name = null;
+            switch (digt)
+            {
+                case 10:
+                    name = "Ten";
+                    break;
+                case 11:
+                    name = "Eleven";
+                    break;
+                case 12:
+                    name = "Twelve";
+                    break;
+                case 13:
+                    name = "Thirteen";
+                    break;
+                case 14:
+                    name = "Fourteen";
+                    break;
+                case 15:
+                    name = "Fifteen";
+                    break;
+                case 16:
+                    name = "Sixteen";
+                    break;
+                case 17:
+                    name = "Seventeen";
+                    break;
+                case 18:
+                    name = "Eighteen";
+                    break;
+                case 19:
+                    name = "Nineteen";
+                    break;
+                case 20:
+                    name = "Twenty";
+                    break;
+                case 30:
+                    name = "Thirty";
+                    break;
+                case 40:
+                    name = "Fourty";
+                    break;
+                case 50:
+                    name = "Fifty";
+                    break;
+                case 60:
+                    name = "Sixty";
+                    break;
+                case 70:
+                    name = "Seventy";
+                    break;
+                case 80:
+                    name = "Eighty";
+                    break;
+                case 90:
+                    name = "Ninety";
+                    break;
+                default:
+                    if (digt > 0)
+                    {
+                        name = tens(digit.Substring(0, 1) + "0") + " " + ones(digit.Substring(1));
+                    }
+                    break;
+            }
+            return name;
+        }
+        private String ones(String digit)
+        {
+            int digt = Convert.ToInt32(digit);
+            String name = "";
+            switch (digt)
+            {
+                case 1:
+                    name = "One";
+                    break;
+                case 2:
+                    name = "Two";
+                    break;
+                case 3:
+                    name = "Three";
+                    break;
+                case 4:
+                    name = "Four";
+                    break;
+                case 5:
+                    name = "Five";
+                    break;
+                case 6:
+                    name = "Six";
+                    break;
+                case 7:
+                    name = "Seven";
+                    break;
+                case 8:
+                    name = "Eight";
+                    break;
+                case 9:
+                    name = "Nine";
+                    break;
+            }
+            return name;
+        }
+        private String translateCents(String cents)
+        {
+            String cts = "", digit = "", engOne = "";
+            for (int i = 0; i < cents.Length; i++)
+            {
+                digit = cents[i].ToString();
+                if (digit.Equals("0"))
+                {
+                    engOne = "Zero";
+                }
+                else
+                {
+                    engOne = ones(digit);
+                }
+                cts += " " + engOne;
+            }
+            return cts;
+        }
+    }
+
 }
---
+++ readme.txt
@@ -0,0 +1,12 @@
+
+1.Rightclick select Ubload Project
+2.Change Debug to Release
+
+goto https://www.nuget.org/downloads get nuget.exe copy in to project
+plass comman 
+nuget spec <Projectname>.csproj exp.>> nuget spec Sleepeye.MVC.csproj
+edit file Sleepeye.MVC.nuspec
+
+plass comman nuget pack 
+
+upload file to nuget web
